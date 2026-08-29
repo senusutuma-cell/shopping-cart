f7f8e82688864db3d7f506716c0628ec81b9cb02
senusutuma-cell <Senusutuma@gmail.com>
2026-08-29 19:59:51 +0300

    improve 404 page with link back to home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,12 @@
-function NotFound() {
-  return <h1>404 - Page Not Found</h1>;
-}
+import { Link } from "react-router-dom";
 
+ function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "4rem" }}>
+      <h1>404</h1>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+ }
 export default NotFound;
